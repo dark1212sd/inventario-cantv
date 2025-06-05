e82495a7ccde11e6ec01a1466d75febe59da672a
Leonardo_21 <emerson1011@outlook.es>
2025-06-05 17:40:51 -0400

    Changes /05/06/25 V3

--- gestion_activos/utils/decoradores.py
+++ gestion_activos/utils/decoradores.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import redirect
+
 
 def grupo_requerido(nombre_grupo):
     def check(user):
         return user.is_authenticated and user.groups.filter(name=nombre_grupo).exists()
-    return user_passes_test(check)
+
+    return user_passes_test(check, login_url='sin_permisos')
