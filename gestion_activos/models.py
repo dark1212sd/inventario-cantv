4b4f7cfd9eb26d20334f20bde8ba9ba7c43070e8
Leonardo_21 <emerson1011@outlook.es>
2025-06-16 18:46:32 -0400

    Changes /16/06/25 V5 solution-table + add user

--- gestion_activos/models.py
+++ gestion_activos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     nombre      = models.CharField("Categoría", max_length=100, unique=True)
@@ -15,19 +16,20 @@ class Ubicacion(models.Model):
         return self.nombre
 
 class Activo(models.Model):
-    codigo = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=50, unique=True)
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
-    ubicacion = models.ForeignKey('Ubicacion', on_delete=models.CASCADE)
-    ESTADOS = [
+    descripcion = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    ubicacion = models.ForeignKey(Ubicacion, on_delete=models.SET_NULL, null=True, blank=True)
+    estado = models.CharField(max_length=20, choices=[
         ('activo', 'Activo'),
-        ('mant', 'En mantenimiento'),
-        ('baja', 'Dado de baja'),
-    ]
+        ('inactivo', 'Inactivo'),
+        ('en_mantenimiento', 'En mantenimiento')
+    ])
+    fecha_registro = models.DateTimeField(auto_now_add=True)
 
-    estado = models.CharField(max_length=10, choices=ESTADOS, default='activo')
-    fecha_registro = models.DateField(auto_now_add=True)  # <- esto hace que se genere automáticamente
+    # Nuevo campo
+    responsable = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='activos_responsables')
 
     def __str__(self):
-        return self.nombre
+        return f'{self.codigo} - {self.nombre}'
