74d54cc398e220e383fb1b8865086ed4edc1ddde
Leonardo_21 <emerson1011@outlook.es>
2025-06-17 15:34:20 -0400

    Changes /17/06/25 => Update email seend V2

--- inventario/urls.py
+++ inventario/urls.py
@@ -23,7 +23,6 @@ from django.urls import path, include
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,9 +39,4 @@ urlpatterns = [
 
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
-    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-
 ]
