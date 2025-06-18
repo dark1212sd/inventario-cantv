e5859edcc68e38c2d2b269213f36284db7b42cf7
Leonardo_21 <emerson1011@outlook.es>
2025-06-18 09:54:50 -0400

    Changes /17/06/25 => login + restore password update

--- inventario/urls.py
+++ inventario/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from gestion_activos import views as custom_views
 
-
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestion_activos.urls')),
     path('accounts/', include('allauth.urls')),
+    path('recuperar/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('recuperar/enviado/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('recuperar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('recuperar/completo/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
